8630610e96f16f01213cd4ad0a590530fd2995d7
JFSIII <JFSIII@GMail.com>
2012-09-29 12:18:23 -0700

    Update the README

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 (function () {
 /**
- *  The [[XCSSMatrix]] class is intended to be a spec-compliant implementation
- *  of the `CSSMatrix` interface defined in the [CSS 2D Transforms][2d] and
- *  [CSS 3D Transforms][3d] Module specifications.
- *
- *  [2d]: http://www.w3.org/TR/2011/WD-css3-2d-transforms-20111215/#cssmatrix-interface
- *  [3d]: http://www.w3.org/TR/2009/WD-css3-3d-transforms-20090320/#cssmatrix-interface
- *
- *  The implementation was largely copied from [Firmin][firmin]'s
- *  `FirminCSSMatrix` object.
- *  [firmin]: [http://extralogical.net/projects/firmin/]
- *
- *  Its API is intended to match the spec. Tests have been copied from the
- *  WebKit tests for [2D](2dtests) and [3D](3dtests). Please create tickets
- * (and/or tests) for any failing cases.
- * [2dtests]: [http://src.chromium.org/svn/branches/WebKit/472/LayoutTests/transforms/cssmatrix-2d-interface.xhtml]
- * [3dtests]: [http://src.chromium.org/svn/branches/WebKit/472/LayoutTests/transforms/cssmatrix-3d-interface.xhtml]
  *  @constructor XCSSMatrix
  *  @param {string} domstr - a string representation of a 2D or 3D transform matrix
  *    in the form given by the CSS transform property, i.e. just like the
